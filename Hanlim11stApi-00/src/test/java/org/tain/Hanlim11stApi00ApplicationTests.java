baccb485122e33915f99f87faac798a3d0706a61
gksruf2848 <4455qqq@naver.com>
2022-04-05 00:52:36 +0900

    commit

---
+++ Hanlim11stApi-00/src/test/java/org/tain/Hanlim11stApi00ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hanlim11stApi00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
